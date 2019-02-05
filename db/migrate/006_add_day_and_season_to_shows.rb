141b57b81828c9a6385447f0baacc147dab15478
Paul Ashour <paul.ashour@gmail.com>
2019-02-05 00:16:35 +0000

    Automatically backed up by Learn

--- db/migrate/006_add_day_and_season_to_shows.rb
+++ db/migrate/006_add_day_and_season_to_shows.rb
@@ -1,7 +1,7 @@
 class AddDayAndSeasonToShows < ActiveRecord::Migration[5.1]
   
   def change
-    alter shows do |t|
+    alter_table :shows do |t|
       t.string :day
       t.string :season
     end
